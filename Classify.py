eee6698e386b8d8926872d399968450b107ca85d
donno2048 <just4now666666@gmail.com>
2021-12-16 19:32:19 +0200

    use jax not numpy

--- Classify.py
+++ Classify.py
@@ -1,6 +1,7 @@
+from tensorflow import convert_to_tensor
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Flatten, Dense
-from numpy import unique, argmax, array
+from jax.numpy import unique, argmax
 class Classifier(Sequential):
     def __init__(self, images, labels, number_of_neurons = 100, method = "relu", optimizer = "Adam", runs = 1):
         super().__init__([
@@ -10,4 +11,4 @@ class Classifier(Sequential):
         ])
         self.compile(optimizer = optimizer, loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
         self.fit(images, labels, epochs = runs)
-    def predict_label(self, image): return argmax(self.predict(array([image]))[0])
+    def predict_label(self, image): return argmax(self.predict(convert_to_tensor([image]))[0])
