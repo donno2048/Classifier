3ea3e8c44a74b1cf624699c6a93776442cd3bfe7
donno2048 <just4now666666@gmail.com>
2021-12-16 19:56:14 +0200

    remove numpy

--- Classify.py
+++ Classify.py
@@ -1,14 +1,13 @@
 from tensorflow import convert_to_tensor
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Flatten, Dense
-from jax.numpy import unique, argmax
 class Classifier(Sequential):
     def __init__(self, images, labels, number_of_neurons = 100, method = "relu", optimizer = "Adam", runs = 1):
         super().__init__([
             Flatten(input_shape = images.shape[1:]),
             Dense(number_of_neurons, activation = method),
-            Dense(unique(labels).size, activation = "softmax")
+            Dense(len(set(labels)), activation = "softmax")
         ])
         self.compile(optimizer = optimizer, loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
         self.fit(images, labels, epochs = runs)
-    def predict_label(self, image): return argmax(self.predict(convert_to_tensor([image]))[0])
+    def predict_label(self, image): return self.predict(convert_to_tensor([image]))[0].argmax()
