491b3014e33803dafd2e3cda8696036097d083bf
donno2048 <just4now666666@gmail.com>
2022-05-12 14:38:28 +0300

    update to function

--- Classify.py
+++ Classify.py
@@ -1,13 +1,11 @@
 from tensorflow import convert_to_tensor
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Flatten, Dense
-class Classifier(Sequential):
-    def __init__(self, images, labels, number_of_neurons = 100, method = "relu", optimizer = "Adam", runs = 1):
-        super().__init__([
-            Flatten(input_shape = images.shape[1:]),
-            Dense(number_of_neurons, activation = method),
-            Dense(len(set(labels)), activation = "softmax")
-        ])
-        self.compile(optimizer = optimizer, loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
-        self.fit(images, labels, epochs = runs)
-    def predict_label(self, image): return self.predict(convert_to_tensor([image]))[0].argmax()
+def Classifier(images, labels, number_of_neurons = 100, method = "relu", optimizer = "Adam", runs = 1):
+    model = Sequential()
+    model.add(Flatten(input_shape = images.shape[1:]))
+    model.add(Dense(number_of_neurons, activation = method))
+    model.add(Dense(len(set(labels)), activation = "softmax"))
+    model.compile(optimizer = optimizer, loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
+    model.fit(images, labels, epochs = runs)
+    return lambda image: model.predict(convert_to_tensor([image]))[0].argmax()
